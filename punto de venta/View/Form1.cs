401d5274ccb203786ac2b329c10ff92e9f11c1d8
luis <luis.roldan294@pascualbravo.edu.co>
2025-11-12 09:17:21 -0500

    Agrege diseño venta

--- punto de venta/View/Form1.cs
+++ punto de venta/View/Form1.cs
@@ -1,3 +1,5 @@
+using punto_de_venta.View;
+
 namespace punto_de_venta
 {
     public partial class Form1 : Form
@@ -5,6 +7,13 @@ namespace punto_de_venta
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 nuevoFormulario = new Form2();
+            nuevoFormulario.Show();
+            this.Hide(); // Oculta el formulario actual
         }
     }
 }
---
+++ punto de venta/View/Form2.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace punto_de_venta.View
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            Codigo = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Unidades = new DataGridViewTextBoxColumn();
+            Precio = new DataGridViewTextBoxColumn();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Codigo, Nombre, Unidades, Precio });
+            dataGridView1.Location = new Point(-65, 8);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(878, 385);
+            dataGridView1.TabIndex = 5;
+            // 
+            // Codigo
+            // 
+            Codigo.HeaderText = "Codigo";
+            Codigo.MinimumWidth = 6;
+            Codigo.Name = "Codigo";
+            Codigo.Width = 125;
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.MinimumWidth = 6;
+            Nombre.Name = "Nombre";
+            Nombre.Width = 125;
+            // 
+            // Unidades
+            // 
+            Unidades.HeaderText = "Unidades";
+            Unidades.MinimumWidth = 6;
+            Unidades.Name = "Unidades";
+            Unidades.Width = 125;
+            // 
+            // Precio
+            // 
+            Precio.HeaderText = "Precio";
+            Precio.MinimumWidth = 6;
+            Precio.Name = "Precio";
+            Precio.Width = 125;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.BackColor = SystemColors.ButtonHighlight;
+            radioButton2.Location = new Point(260, 418);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(184, 24);
+            radioButton2.TabIndex = 9;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Productos_Proveedores";
+            radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.BackColor = SystemColors.ButtonFace;
+            radioButton1.BackgroundImageLayout = ImageLayout.Stretch;
+            radioButton1.Location = new Point(30, 418);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(77, 24);
+            radioButton1.TabIndex = 8;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Facture";
+            radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ButtonFace;
+            label2.Location = new Point(737, 418);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 20);
+            label2.TabIndex = 7;
+            label2.Text = "$";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Location = new Point(616, 418);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Total";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(939, 471);
+            Controls.Add(dataGridView1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Codigo;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Unidades;
+        private DataGridViewTextBoxColumn Precio;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label2;
+        private Label label1;
+    }
+}
